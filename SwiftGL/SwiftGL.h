710e8045564931f1a634e0599375210526a8d10c
Scott Bennett <scottbennett912@gmail.com>
2014-06-08 14:26:15 -0500

    Initial Commit

---
+++ SwiftGL/SwiftGL.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGL.h
+//  SwiftGL
+//
+//  Created by Scott Bennett on 2014-06-08.
+//  Copyright (c) 2014 Project Catalyst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftGL.
+FOUNDATION_EXPORT double SwiftGLVersionNumber;
+
+//! Project version string for SwiftGL.
+FOUNDATION_EXPORT const unsigned char SwiftGLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGL/PublicHeader.h>
+
+
