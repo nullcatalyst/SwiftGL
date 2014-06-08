99175dbfe29b91015161a74d007a0592c3ce951b
Scott Bennett <scottbennett912@gmail.com>
2014-06-08 15:05:00 -0500

    Initial Vec2 implementation

---
+++ SwiftGL/Vec2.swift
@@ -0,0 +1,95 @@
+//
+//  Vec2.swift
+//  SwiftGL
+//
+//  Created by Scott Bennett on 2014-06-08.
+//  Copyright (c) 2014 Project Catalyst. All rights reserved.
+//
+
+import Foundation
+
+extension Vec2 {
+    init() {
+        self.x = 0
+        self.y = 0
+    }
+    
+    init(s: CFloat) {
+        self.x = s
+        self.y = s
+    }
+    
+    init(x: CFloat, y: CFloat) {
+        self.x = x
+        self.y = y
+    }
+    
+    var length2: CFloat {
+    get {
+        return x * x + y * y
+    }
+    }
+    
+    var length: CFloat {
+    get {
+        return sqrtf(self.length2)
+    }
+    set(l) {
+        self = l * normalize(self)
+    }
+    }
+    
+    // Swizzle (Vec2) Properties
+    var xx: Vec2 {get {return Vec2(x: x, y: x)}}
+    var xy: Vec2 {get {return Vec2(x: x, y: y)} set(v) {self.x = v.x; self.y = v.y}}
+    var yx: Vec2 {get {return Vec2(x: y, y: x)} set(v) {self.y = v.x; self.x = v.y}}
+    var yy: Vec2 {get {return Vec2(x: y, y: y)}}
+    
+    // Swizzle (Vec3) Properties
+    var xxx: Vec3 {get {return Vec3(x: x, y: x, z: x)}}
+    var xxy: Vec3 {get {return Vec3(x: x, y: x, z: y)}}
+    var xyx: Vec3 {get {return Vec3(x: x, y: y, z: x)}}
+    var xyy: Vec3 {get {return Vec3(x: x, y: y, z: y)}}
+    var yxx: Vec3 {get {return Vec3(x: y, y: x, z: x)}}
+    var yxy: Vec3 {get {return Vec3(x: y, y: x, z: y)}}
+    var yyx: Vec3 {get {return Vec3(x: y, y: y, z: x)}}
+    var yyy: Vec3 {get {return Vec3(x: y, y: y, z: y)}}
+    
+    // Swizzle (Vec4) Properties
+    var xxxx: Vec4 {get {return Vec4(x: x, y: x, z: x, w: x)}}
+    var xxxy: Vec4 {get {return Vec4(x: x, y: x, z: x, w: y)}}
+    var xxyx: Vec4 {get {return Vec4(x: x, y: x, z: y, w: x)}}
+    var xxyy: Vec4 {get {return Vec4(x: x, y: x, z: y, w: y)}}
+    var xyxx: Vec4 {get {return Vec4(x: x, y: y, z: x, w: x)}}
+    var xyxy: Vec4 {get {return Vec4(x: x, y: y, z: x, w: y)}}
+    var xyyx: Vec4 {get {return Vec4(x: x, y: y, z: y, w: x)}}
+    var xyyy: Vec4 {get {return Vec4(x: x, y: y, z: y, w: y)}}
+    var yxxx: Vec4 {get {return Vec4(x: y, y: x, z: x, w: x)}}
+    var yxxy: Vec4 {get {return Vec4(x: y, y: x, z: x, w: y)}}
+    var yxyx: Vec4 {get {return Vec4(x: y, y: x, z: y, w: x)}}
+    var yxyy: Vec4 {get {return Vec4(x: y, y: x, z: y, w: y)}}
+    var yyxx: Vec4 {get {return Vec4(x: y, y: y, z: x, w: x)}}
+    var yyxy: Vec4 {get {return Vec4(x: y, y: y, z: x, w: y)}}
+    var yyyx: Vec4 {get {return Vec4(x: y, y: y, z: y, w: x)}}
+    var yyyy: Vec4 {get {return Vec4(x: y, y: y, z: y, w: y)}}
+}
+
+// Vec2 Prefix Operators
+@prefix func - (v: Vec2) -> Vec2 {return Vec2(x: -v.x, y: -v.y)}
+
+// Vec2 Infix Operators
+@infix func + (a: Vec2, b: Vec2) -> Vec2 {return Vec2(x: a.x + b.x, y: a.y + b.y)}
+@infix func - (a: Vec2, b: Vec2) -> Vec2 {return Vec2(x: a.x - b.x, y: a.y - b.y)}
+@infix func * (a: Vec2, b: Vec2) -> Vec2 {return Vec2(x: a.x * b.x, y: a.y * b.y)}
+@infix func / (a: Vec2, b: Vec2) -> Vec2 {return Vec2(x: a.x / b.x, y: a.y / b.y)}
+
+// Vec2 Scalar Operators
+@infix func * (s: CFloat, v: Vec2) -> Vec2 {return Vec2(x: s * v.x, y: s * v.y)}
+@infix func * (v: Vec2, s: CFloat) -> Vec2 {return Vec2(x: v.x * s, y: v.y * s)}
+@infix func / (v: Vec2, s: CFloat) -> Vec2 {return Vec2(x: v.x / s, y: v.y / s)}
+
+// Functions which operate on Vec2
+func length(v: Vec2) -> CFloat {return v.length}
+func length2(v: Vec2) -> CFloat {return v.length2}
+func normalize(v: Vec2) -> Vec2 {return v / v.length}
+func mix(a: Vec2, b: Vec2, t: CFloat) -> Vec2 {return a + (b - a) * t}
--- SwiftGLTests/SwiftGLTests.swift
+++ SwiftGLTests/SwiftGLTests.swift
@@ -9,7 +9,6 @@
 import XCTest
 
 class SwiftGLTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -31,5 +30,4 @@ class SwiftGLTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
 }
