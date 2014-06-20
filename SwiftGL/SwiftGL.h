04beb10ff403b44e41602acb7f1806dad10ac242
Scott Bennett <scottbennett912@gmail.com>
2014-06-19 19:53:17 -0500

    Modified the bridging header for easy porting to iOS

--- SwiftGL/SwiftGL.h
+++ SwiftGL/SwiftGL.h
@@ -16,4 +16,27 @@ FOUNDATION_EXPORT const unsigned char SwiftGLVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftGL/PublicHeader.h>
 
-#import <OpenGL/gl3.h>
+#include <Availability.h>
+#ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
+    #include <OpenGLES/ES2/gl.h>
+    #include <OpenGLES/ES2/glext.h>
+
+    // Redefine the functions that have different names so that we can use the exact same code
+    #define glGenVertexArrays           glGenVertexArraysOES
+    #define glDeleteVertexArrays        glDeleteVertexArraysOES
+    #define glBindVertexArray           glBindVertexArrayOES
+    #define glVertexAttribDivisor       glVertexAttribDivisorEXT
+    #define glProgramUniform1i          glProgramUniform1iEXT
+    #define glProgramUniform1f          glProgramUniform1fEXT
+    #define glProgramUniform2f          glProgramUniform2fEXT
+    #define glProgramUniform3f          glProgramUniform3fEXT
+    #define glProgramUniform4f          glProgramUniform4fEXT
+    #define glProgramUniform1fv         glProgramUniform1fvEXT
+    #define glProgramUniform2fv         glProgramUniform2fvEXT
+    #define glProgramUniform3fv         glProgramUniform3fvEXT
+    #define glProgramUniform4fv         glProgramUniform4fvEXT
+    #define glProgramUniformMatrix4fv   glProgramUniformMatrix4fvEXT
+#else /* defined(__IPHONE_OS_VERSION_MIN_REQUIRED) */
+    #include <OpenGL/gl3.h>
+    #include <OpenGL/gl3ext.h>
+#endif /* defined(__IPHONE_OS_VERSION_MIN_REQUIRED) */
